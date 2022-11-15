3f41000a633bd7ebe44246ec1423688c4f4cfc84
laksh0786 <112651015+laksh0786@users.noreply.github.com>
2022-11-15 18:18:30 +0530

    Update sum.cpp

--- Lecture033 Recursion Day3/sum.cpp
+++ Lecture033 Recursion Day3/sum.cpp
@@ -7,13 +7,19 @@ int getSum(int *arr, int size) {
     if(size == 0) {
         return 0;
     }
-    if(size == 1 )
-    {
-        return arr[0];
-    }
+//     if(size == 1 )
+//     {
+//         return arr[0];
+//     }
 
-    int remainingPart = getSum(arr+1, size-1);
-    int sum = arr[0] + remainingPart;
+//     int remainingPart = getSum(arr+1, size-1);
+//     int sum = arr[0] + remainingPart;
+//     return sum;
+     
+    
+    //Recursive call
+    int sum=a[0];
+    sum=sum+getSum( arr+1 , size-1);
     return sum;
 }
 
